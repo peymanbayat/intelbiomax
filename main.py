8518afb010b4a7102904b3e410fa166ddbca13ad
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-06-27 12:44:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def predict():
     bmi = st.number_input("Body mass index (weight in kg/(height in m)^2):")
     st.markdown('BMI: Body mass index (weight in kg/(height in m)^2)')
 
-    choices = {1: "Aminoglycosides", 2: "Macrolidesb", 3: "Tetracyclines", 2: "Lincomycins"}
+    CHOICES = {1: "Aminoglycosides", 2: "Macrolidesb", 3: "Tetracyclines", 2: "Lincomycins"}
     def format_func(dpf):
     return CHOICES[dpf]
     dpf = st.selectbox(
